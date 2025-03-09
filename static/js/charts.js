d17e071fa5c98901835cb01e2488e9733470ba63
Sebastiano Giacomini <sebastiano.giacomini@libero.it>
2025-03-09 19:25:24 +0100

    updated data viz css

--- static/js/charts.js
+++ static/js/charts.js
@@ -15,9 +15,7 @@ $(document).ready(function() {
         var rect = el.getBoundingClientRect();
         return (
             rect.top >= 0 && 
-            rect.left >= 0 && 
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            rect.left >= 0
         );
     }
 
